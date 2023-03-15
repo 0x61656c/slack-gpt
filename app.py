c715d659daa607cb771f7fd6fa7e496e35567c99
Aaron Lebel <37777357+alebml@users.noreply.github.com>
2023-03-15 18:58:24 -0400

    swithc to socket mode

--- app.py
+++ app.py
@@ -2,12 +2,15 @@
 import os
 import re
 from flask import Flask, request
-import slack
-from slack import WebClient
-from slackeventsapi import SlackEventAdapter
+import slack_sdk
+from slack_sdk import WebClient
+from slack_sdk.socket_mode import SocketModeClient
+from slack_sdk.socket_mode.request import SocketModeRequest
+from slack_sdk.socket_mode.response import SocketModeResponse
 import openai
 
 # Set your API tokens for Slack and GPT-4 (replace with your actual tokens)
+SLACK_APP_TOKEN = os.environ.get("SLACK_APP_TOKEN")
 SLACK_API_TOKEN = os.environ.get("SLACK_API_TOKEN")
 SLACK_SIGNING_SECRET = os.environ.get("SLACK_SIGNING_SECRET")
 GPT4_API_TOKEN = os.environ.get("GPT4_API_TOKEN")
@@ -15,9 +18,8 @@ GPT4_API_TOKEN = os.environ.get("GPT4_API_TOKEN")
 # Initialize Flask app
 app = Flask(__name__)
 
-# Initialize Slack client and event adapter
+# Initialize Slack client
 slack_client = WebClient(token=SLACK_API_TOKEN)
-slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events", app)
 
 # Initialize GPT-4 API client
 openai.api_key = GPT4_API_TOKEN
@@ -34,10 +36,9 @@ def generate_gpt4_response(prompt):
     )
     return response.choices[0].text.strip()
 
-# Event handler for receiving a message in Slack
-@slack_events_adapter.on("app_mention")
-def handle_message(event_data):
-    event = event_data["event"]
+# Socket Mode event handler for receiving a message in Slack
+def handle_message(payload):
+    event = payload["event"]
     bot_user_id = slack_client.auth_test()["user_id"]
     user_input = event["text"]
 
@@ -46,6 +47,15 @@ def handle_message(event_data):
         gpt4_response = generate_gpt4_response(user_input)
         slack_client.chat_postMessage(channel=event["channel"], text=gpt4_response)
 
-# Start the Flask app
+# Socket Mode event listener
+def process_socket_mode_request(client: SocketModeClient, req: SocketModeRequest):
+    if req.type == "events_api":
+        if req.payload["event"]["type"] == "app_mention":
+            handle_message(req.payload)
+        client.send_socket_mode_response(SocketModeResponse(envelope_id=req.envelope_id))
+
+# Initialize and start the Socket Mode client
 if __name__ == "__main__":
-    app.run(port=int(os.environ.get("PORT", 3000)), debug=True)
+    socket_mode_client = SocketModeClient(app_token=SLACK_APP_TOKEN)
+    socket_mode_client.socket_mode_request_listeners.append(process_socket_mode_request)
+    socket_mode_client.connect()
