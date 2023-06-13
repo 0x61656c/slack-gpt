978c7b147773b38e8827399ff29060fbd26cc24a
Aaron Lebel <37777357+alebml@users.noreply.github.com>
2023-06-13 15:09:46 -0400

    update

--- app.py
+++ app.py
@@ -57,18 +57,18 @@ def handle_message(event_data):
         current_day = datetime.now().strftime('%A')
         if current_day not in ['Monday', 'Friday']:
             if current_day in ['Saturday', 'Sunday']:
-                slack_response = "Aaron is currently unavailable for support. His next available complimentary support window is 9am-5pm on Monday. If you need help with an urgent bug, please file a support ticket here: https://form.typeform.com/to/TWWlou8R"
+                slack_response = "<@{user_id}> Aaron is currently unavailable for support--his next available complimentary support window is 9am-5pm on Monday. If you need help with an urgent bug, please file a support ticket here: https://form.typeform.com/to/TWWlou8R"
             else:
-                slack_response = "Aaron is currently unavailable for support. His next available complimentary support window is 9am-5pm on Friday. If you need help with an urgent bug, please file a support ticket here: https://form.typeform.com/to/TWWlou8R"
+                slack_response = "<@{user_id}> Aaron is currently unavailable for support--his next available complimentary support window is 9am-5pm on Friday. If you need help with an urgent bug, please file a support ticket here: https://form.typeform.com/to/TWWlou8R"
             slack_client.chat_postMessage(channel=channel, text=slack_response, thread_ts=thread_ts)
 
     if re.search("U012SDDLX8E", user_input):
         current_day = datetime.now().strftime('%A')
         if current_day not in ['Monday', 'Friday']:
             if current_day in ['Saturday', 'Sunday']:
-                slack_response = "Paris is currently unavailable for support. Her next available complimentary support window is 9am-5pm on Monday. If you need help with an urgent bug, please file a support ticket here: https://form.typeform.com/to/TWWlou8R"
+                slack_response = "Paris is currently unavailable for support--Her next available complimentary support window is 9am-5pm on Monday. If you need help with an urgent bug, please file a support ticket here: https://form.typeform.com/to/TWWlou8R"
             else:
-                slack_response = "Paris is currently unavailable for support. Her next available complimentary support window is 9am-5pm on Friday. If you need help with an urgent bug, please file a support ticket here: https://form.typeform.com/to/TWWlou8R"
+                slack_response = "Paris is currently unavailable for support--Her next available complimentary support window is 9am-5pm on Friday. If you need help with an urgent bug, please file a support ticket here: https://form.typeform.com/to/TWWlou8R"
             slack_client.chat_postMessage(channel=channel, text=slack_response, thread_ts=thread_ts)
 
 # Start the Flask app
